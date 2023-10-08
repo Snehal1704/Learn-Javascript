d2eef3025abfb7b2b03e10e366d760711378af9a
Snehal Ghanekar <138196946+Snehal1704@users.noreply.github.com>
2023-10-08 15:14:14 +0000

    First Commit

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let Marks = "Snehal"
+
+// console.log(typeof Marks);
+// console.log(typeof(Marks));
+
+let valueInNumber = Number(Marks)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "90" => 90
+// "90abc" => NaN  (Not a Number)
+//  true => 1; false => 0
+
+let isLoggedIn = "Snehal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Snehal" => true
+
+let someNumber = 90
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
